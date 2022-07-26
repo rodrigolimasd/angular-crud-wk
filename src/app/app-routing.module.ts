24fe23476ff70d79e5763fcc839748b9858a90a3
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-26 16:27:44 -0300

    feat: list of fake products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produtos',
+    component: ProdutosComponent,
+    data: { title: 'Produtos'}
+  },
+  {
+    path: '',
+    redirectTo: '/produtos',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
+  produtos: any = [{descricao:"teste 1"},{descricao:"teste 2"}]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  edit(produto: any) {
+
+  }
+
+  delete(produto: any) {
+    
   }
 
 }
